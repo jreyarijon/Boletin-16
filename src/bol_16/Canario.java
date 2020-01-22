d6d9931cbc53ad6adb372f2e949c131aa7c89f5e
unknown <jreyarijon@danielcastelao.org>
2020-01-22 10:45:56 +0100

    prog interfaces

---
+++ src/bol_16/Canario.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol_16;
+
+/**
+ *
+ * @author Josemolamazo
+ */
+public class Canario implements IpodeCantar {
+
+    @Override
+    public void Cantar() {
+        System.out.println("Pio pio");
+    }
+}
---
+++ src/bol_16/Galo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bol_16;
+
+/**
+ *
+ * @author Josemolamazo
+ */
+public class Galo implements IpodeCantar{
+    @Override
+    public void Cantar(){
+        System.out.println("ki kiri ki");
+    }
+}
